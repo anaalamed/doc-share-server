5ca2e53dc32a5bd964a408f64501a52404d99adf
talbenzur <tbz1996@gmail.com>
2022-11-18 17:26:59 +0200

    doc controller delete function

--- src/main/java/docSharing/controller/DocumentController.java
+++ src/main/java/docSharing/controller/DocumentController.java
@@ -7,33 +7,63 @@ import docSharing.service.DocumentService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.swing.text.StyledEditorKit;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class DocumentController {
-    private static DocumentService documentService=new DocumentService();
+    private static DocumentService documentService = new DocumentService();
     private static Logger logger = LogManager.getLogger(DocumentController.class.getName());
 
-    public static BaseResponse<Document>  create(User owner,String title){
-        if(isUserNull(owner)){
+    public static BaseResponse<Document> create(User owner, String title) {
+        if (isUserNull(owner)) {
             logger.error("in create: NULL user trying to create file!");
         }
-        if(title==""){
+        if (title == "") {
             logger.info("in create: The document will be created without a title");
         }
 
-       Document document=documentService.create( owner,title);//TODO: create implementation in service
-        if (document!= null){
+        Document document = documentService.create(owner, title);//TODO: create implementation in service
+        if (document != null) {
             return BaseResponse.success(document);
-        }else{
+        } else {
             return BaseResponse.failure("Null document");
         }
-
     }
 
-    private static Boolean isUserNull(User user){
-        if(user.getId()==0)
+    public static BaseResponse<Boolean> delete(String url, User user) {
+        boolean isDeleted=false;
+        if (!isUserOwner(url, user)) {
+            logger.error("in delete: -owner foreigner. No delete permission!");
+        } else if(!isValidURL(url)) {
+                logger.error("in delete: Invalid url!");
+            }else {
+                if (documentService.delete(url, user)) {//TODO: delete implementation in service
+                    return BaseResponse.success(true);
+                }
+            }
+       return BaseResponse.failure("Error. No deletion was performed.");
+    }
+
+    private static Boolean isUserOwner (String url, User user){
+        //TODO:implement
+    }
+    private static Boolean isUserNull (User user){
+        if (user.getId() == 0)
             return true;
         return false;
     }
-
+    private static boolean isValidURL(String url) {
+        try {
+            new URL(url).toURI();
+            return true;
+        } catch (MalformedURLException e) {
+            return false;
+        } catch (URISyntaxException e) {
+            return false;
+        }
+    }
 
 }
 /*API:
