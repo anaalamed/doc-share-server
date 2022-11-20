ffbf45dc499a7f6fea62bb85129db6cfbe24a61d
talbenzur <tbz1996@gmail.com>
2022-11-20 14:30:59 +0200

    separate between folder and document in FileController

---
+++ src/main/java/docSharing/controller/DocumentEditController.java
@@ -0,0 +1,56 @@
+package docSharing.controller;
+
+import docSharing.controller.request.UpdateRequest;
+import docSharing.entities.User;
+import docSharing.service.DocumentService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import static docSharing.utils.Utils.*;
+
+@Controller
+@ComponentScan
+public class DocumentEditController {
+    @Autowired
+    private DocumentService documentService;//check if needed?
+
+    private static final Logger logger = LogManager.getLogger(DocumentEditController.class.getName());
+
+    public DocumentEditController() {
+    }
+
+    @MessageMapping("/join")
+    public void join(User user,String url){
+        logger.info("in join");
+        if(!isValidURL(url)){
+            logger.error("Invalid URL!");
+        }
+        documentService.join(user,url);
+        logger.info("user"+ user.getName() +"join to:"+ url);
+    }
+
+    @MessageMapping("/leave") //TODO: add path 'leave' in client
+    public void leave(User user, String url) {
+        logger.info("in leave");
+        if(!isValidURL(url)){
+            logger.error("Invalid URL!");
+        }
+        documentService.leave(user,url);
+        logger.info("user"+ user.getName() +"leave:"+ url);
+    }
+
+    @MessageMapping("/update")
+    @SendTo("/topic/updates")
+    public void update(String url, UpdateRequest updateRequest){
+        logger.info("in update");
+        if(!isValidURL(url)){
+            logger.error("Invalid URL!");
+        }
+        documentService.update(url, updateRequest);
+        logger.info("update message:"+updateRequest.getContent()+"in url:"+url);
+    }
+}
--- src/main/java/docSharing/controller/FolderController.java
+++ src/main/java/docSharing/controller/FolderController.java
@@ -6,27 +6,25 @@ import docSharing.entities.User;
 import docSharing.entities.document.Document;
 import docSharing.entities.document.FileType;
 import docSharing.entities.document.Folder;
-import docSharing.service.DocumentService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import static docSharing.utils.Utils.isCreateValid;
-import static docSharing.utils.Utils.isValidURL;
+import static docSharing.utils.Utils.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/document")
-public class DocumentController {
+@RequestMapping("/folder")
+public class FolderController {
     @Autowired
-    private DocumentService documentService;
+    private FolderService folderService;
 
-    private static final Logger logger = LogManager.getLogger(DocumentController.class.getName());
+    private static final Logger logger = LogManager.getLogger(FolderController.class.getName());
 
-    public DocumentController() {
+    public FolderController() {
     }
+
 
     @RequestMapping(method = RequestMethod.POST, path="/create")
     public ResponseEntity<BaseResponse<Document>> create(@RequestHeader int id, @RequestParam User owner,
@@ -37,26 +35,15 @@ public class DocumentController {
         }
         return ResponseEntity.ok(BaseResponse.success(
                 true,
-                "document: "+title+"created",
-                documentService.create(owner, parent, title, id)));
-    }
-
-    @RequestMapping(method = RequestMethod.PATCH, path="/updatePermission/{permission}")
-    public ResponseEntity<BaseResponse<User>> updatePermission(@RequestHeader int id, @RequestParam User owner,
-                                                                 @RequestParam User user, @PathVariable("permission") Permission permission){
-        logger.info("in updatePermission");
-
-        return ResponseEntity.ok(BaseResponse.success(
-                true,
-                "owner: "+owner.getName()+"update "+ user.getName()+"permission to: "+ permission,
-                documentService.updatePermission(id,owner, user, permission)));
+                "folder: "+title+"created",
+                folderService.create(owner, parent, title, id)));
     }
 
     @RequestMapping(method = RequestMethod.DELETE,path="/delete")
     public ResponseEntity<BaseResponse<Void>> delete(@RequestHeader int id, @RequestParam User user){
         logger.info("in delete");
-        documentService.delete(id,user);
-        return ResponseEntity.ok(BaseResponse.noContent(true,"document deleted"));
+        folderService.delete(id,user);
+        return ResponseEntity.ok(BaseResponse.noContent(true,"folder deleted"));
     }
 
 
